eb69e86358403e1e652c5310ccfd6825b9fae60b
eeeeeaaan <wjdwjddla73@naver.com>
2026-06-28 14:51:56 +0900

    init Project

---
+++ src/main/java/com/honjaopseoyae/HonjaopseoyaeApplication.java
@@ -0,0 +1,13 @@
+package com.honjaopseoyae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonjaopseoyaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonjaopseoyaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honjaopseoyae/HonjaopseoyaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honjaopseoyae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonjaopseoyaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
