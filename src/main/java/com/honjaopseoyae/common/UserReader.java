f7c76ff6ff2d17ddbdbf51b064908c38137827a5
eeeeeaaan <wjdwjddla73@naver.com>
2026-07-13 14:17:39 +0900

    fix : user Repo 통합

--- src/main/java/com/honjaopseoyae/common/UserReader.java
+++ src/main/java/com/honjaopseoyae/common/UserReader.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import com.honjaopseoyae.domain.user.entity.User;
 import com.honjaopseoyae.global.apipayload.domain.UserErrorStatus;
 import com.honjaopseoyae.global.apipayload.exception.GeneralException;
-import com.honjaopseoyae.member.repository.UserRepository;
+import com.honjaopseoyae.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
